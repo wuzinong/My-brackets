8e78e601462ce1f613bcef73d269068d06ba8ed9
Gu <Xie.Hai.Bran.Gu@dnvgl.com>
2018-02-27 15:19:29 +0800

    commit update add axios

--- React/src/views/list/list-component.js
+++ React/src/views/list/list-component.js
@@ -2,23 +2,41 @@ import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
 import listStyle from './list.scss';
 import ListComponent from '../../components/Lists/index.js';
+import axios from 'axios';
 
 class List extends Component{
     constructor(props){
         super(props);
-        this.state={searchValue:"test"};
+        this.state={
+            searchValue:"test",
+            listData:[],
+            listComponent:null
+        };
     }
     search(){ 
-        console.log(this.searchInput.value);
+        let value = this.searchInput.value;
+        let arr = this.state.listData.filter(item=>item.indexOf(value)>=0);
+        this.setState({
+            listComponent:<ListComponent listData={arr}/>
+        });
     }
-    render(){
-        let listData = ["aaa","bbb"];
+    componentDidMount(){
+        axios.get("http://127.0.0.1:10010/data").then((response)=>{
+            this.setState({
+                listData:response.data,
+                listComponent:<ListComponent listData={response.data}/>
+            });
+        }); 
+    }
+    render(){ 
+        
         return <div className={listStyle.listContainer+" mylist"}>
-           <header><Link to="/">back &gt;</Link>This is a test l</header>
+           <header><Link to="/">back &gt;</Link>This is a test list</header>
            <div>
                <input ref={(value)=>this.searchInput = value} 
                       onKeyUp={()=>{this.search()}} type="text" placeholder="search ..."/></div>
-           <ListComponent listData={listData}/>
+           {this.state.listComponent}
+                      
         </div>
     }
 }
--- Swagger/test-data/api/controllers/array_data.js
+++ Swagger/test-data/api/controllers/array_data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function data(req,res){
-    let arr_data = ["test1","test2"];
+    let arr_data = ["test1","test2","test3","test4","test5"];
     res.json(arr_data);
 }
 
