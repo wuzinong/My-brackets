bbc81d479400ec4cce784484e7541eabbf1aae5e
Gu <Xie.Hai.Bran.Gu@dnvgl.com>
2018-06-14 14:34:45 +0800

    update

--- React/src/views/imgUpload/imgUpload-component.js
+++ React/src/views/imgUpload/imgUpload-component.js
@@ -10,30 +10,119 @@ class ImgUpload extends Component{
     constructor(props){
         super(props);
         this.state = {
+            src:"../../assets/images/test.jpg",
             crop: {
-                x: 20,
-                y: 10,
-                width: 30,
-                height: 10
+                aspect: 16/9
               }
         };
+        this.localResource={
+            headIcon:{
+                height:600,
+                width:800
+            }
+        }
     }
+
+    pictureSelected(e) {
+        e.preventDefault();
+        let files;
+        if (e.dataTransfer) {
+            files = e.dataTransfer.files;
+        } else if (e.target) {
+            files = e.target.files;
+        }
+        const reader = new FileReader();
+        reader.onload = () => {
+            let image = new Image();
+            image.src = reader.result;
+            image.onload = () => {
+                let iw = image.width;
+                let ih = image.height;
+                // let scropW;
+                // let scropH;
+                // if (iw * this.localResource.headIcon.height > ih * this.localResource.headIcon.width) {
+                //     scropH = 100;
+                //     scropW = ih * 100 * this.localResource.headIcon.width / this.localResource.headIcon.height / iw;
+                // } else {
+                //     scropW = 100;
+                //     scropH = iw * 100 * this.localResource.headIcon.height / this.localResource.headIcon.width / ih;
+                // }
+                let initCrop = {
+                    aspect: this.localResource.headIcon.width / this.localResource.headIcon.height,
+                    height: 0,
+                    width: 0,
+                    x: 0,
+                    y: 0
+                };
+                this.setState({ crop: initCrop});
+            };
+            
+            this.setState({ src: reader.result, formErrors: [] });
+        };
+        reader.readAsDataURL(files[0]);
+}
+
     
-    onChange(crop){
-        this.setState({crop});
-        console.log(this)
-    }
      
- 
+    cropOnClick(crop, pixelCrop) {
+        let cropAspect = { aspect: this.localResource.headIcon.width / this.localResource.headIcon.height };
+        crop = Object.assign({}, crop, cropAspect);
+        this.setState({ crop: crop });
+    }
+
+    getCroppedImg(imageSrc, percentageCrop, fileName) {
+        if (percentageCrop) {
+            debugger;
+            let image = new Image(imageSrc.width, imageSrc.height);
+            image.src = imageSrc;
+            //let pixelCrop = this.getPixelCrop(image, percentageCrop);
+            let pixelCrop = {
+                x:percentageCrop.x,
+                y:percentageCrop.y,
+                width:image.width,
+                height:image.height
+            }
+            const canvas = document.createElement('canvas');
+            canvas.width = this.localResource.headIcon.width;
+            canvas.height = this.localResource.headIcon.height;
+            const ctx = canvas.getContext('2d');
+
+            ctx.drawImage(
+                image,
+                pixelCrop.x,
+                pixelCrop.y,
+                pixelCrop.width,
+                pixelCrop.height,
+                0,
+                0,
+                this.localResource.headIcon.width,
+                this.localResource.headIcon.height
+            );
+
+            // As Base64 string
+            let base64Image = canvas.toDataURL('image/jpeg');
+            // As a blob
+            //let testImage = new Promise((resolve, reject) => {
+            //    canvas.toBlob(file => { resolve(file);}, 'image/jpeg');
+            //});
+            this.setState({ cropResult: base64Image, formErrors: [] });
+        } else {
+            let errors = [];
+             this.setState({ formErrors: errors });
+        }
+    }
+
+
     render(){
         
         return (
             <div>
-               <ReactCrop 
-                   src={require("../../assets/images/test.jpg")}
-                   onChange={()=>{this.onChange}}
-                   crop = {this.state.crop}
-                   />
+               <input type="file" accept="image/*" onChange={this.pictureSelected.bind(this)} /><br/>
+                   {this.state.src && <ReactCrop onChange={(crop, pixelCrop) => { this.cropOnClick(crop, pixelCrop) }} crop={this.state.crop} src={this.state.src} />}
+                   {this.state.src && <div className='clopBtn'>
+                       <button type="button" className="btn btn-primary padding-right-16" onClick={this.getCroppedImg.bind(this, this.state.src, this.state.crop, '')}>Crop</button>
+                   </div>}
+                  {this.state.cropResult && <img src={this.state.cropResult} alt="cropped image" />}
             </div>
         );
     }
