c0b2d935f6a41b59b81c466be97e2b977ce394c9
Gu <Xie.Hai.Bran.Gu@dnvgl.com>
2018-06-15 09:12:57 +0800

    update\

--- React/src/views/imgUpload/imgUpload-component.js
+++ React/src/views/imgUpload/imgUpload-component.js
@@ -73,15 +73,33 @@ class ImgUpload extends Component{
     getCroppedImg(imageSrc, percentageCrop, fileName) {
         if (percentageCrop) {
             debugger;
-            let image = new Image(imageSrc.width, imageSrc.height);
+            // let image = new Image(imageSrc.width, imageSrc.height);
+            // image.src = imageSrc;
+            // let pixelCrop = {
+            //     x:percentageCrop.x,
+            //     y:percentageCrop.y,
+            //     width:image.width,
+            //     height:image.height
+            // }
+            // const canvas = document.createElement('canvas');
+            // canvas.width = this.localResource.headIcon.width;
+            // canvas.height = this.localResource.headIcon.height;
+            // const ctx = canvas.getContext('2d');
+
+            // ctx.drawImage(
+            //     image,
+            //     pixelCrop.x,
+            //     pixelCrop.y,
+            //     pixelCrop.width,
+            //     pixelCrop.height,
+            //     0,
+            //     0,
+            //     this.localResource.headIcon.width,
+            //     this.localResource.headIcon.height
+            // );
+
+            let image = new Image(percentageCrop.width,percentageCrop.height);
             image.src = imageSrc;
-            //let pixelCrop = this.getPixelCrop(image, percentageCrop);
-            let pixelCrop = {
-                x:percentageCrop.x,
-                y:percentageCrop.y,
-                width:image.width,
-                height:image.height
-            }
             const canvas = document.createElement('canvas');
             canvas.width = this.localResource.headIcon.width;
             canvas.height = this.localResource.headIcon.height;
@@ -89,15 +107,16 @@ class ImgUpload extends Component{
 
             ctx.drawImage(
                 image,
-                pixelCrop.x,
-                pixelCrop.y,
-                pixelCrop.width,
-                pixelCrop.height,
+                percentageCrop.x,
+                percentageCrop.y,
+                percentageCrop.width,
+                percentageCrop.height,
                 0,
                 0,
                 this.localResource.headIcon.width,
                 this.localResource.headIcon.height
             );
+
 
             // As Base64 string
             let base64Image = canvas.toDataURL('image/jpeg');
