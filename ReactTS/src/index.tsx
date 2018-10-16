4b057bd079b42d443a6a94c83cf3aa469903b5df
wuzinong <wuzinong@gmail.com>
2018-10-16 11:03:21 +0800

    update

--- ReactTS/src/index.tsx
+++ ReactTS/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM  from 'react-dom';
 import configureStore from './redux/store/configureStore';
 import App from './router';
-
+import './assets/styles/site.scss'
 
 // ReactDOM.render(
 //     <Home/>,
