4f5634526065bf8221ce1be35da09c9129012591
wuzinong <wuzinong@gmail.com>
2018-10-17 16:35:36 +0800

    update

--- AspDotNet-ReactRedux/WebApplication1/WebApplication1/ClientApp/src/components/Home.js
+++ AspDotNet-ReactRedux/WebApplication1/WebApplication1/ClientApp/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import '../assets/styles/site.scss';
 
 const Home = props => (
   <div>
--- ReactTS/src/router/index.tsx
+++ ReactTS/src/router/index.tsx
@@ -7,18 +7,18 @@ import asyncComponent from '../libs/AsyncComponents';
 
 // import Loading from '../components/loading';
 
-import Hello from '../views/Hello';
-import Home from '../views/Home';
-import NotFound from '../views/NotFound';
-import About from '../views/About';
-import List from '../views/List';
+// import Hello from '../views/Hello';
+// import Home from '../views/Home';
+// import NotFound from '../views/NotFound';
+// import About from '../views/About';
+// import List from '../views/List';
 
 
-// const Hello = asyncComponent(()=>import('../views/Hello'));
-// const Home = asyncComponent(()=>import('../views/Home'));
-// const NotFound = asyncComponent(()=>import('../views/NotFound'));
-// const About = asyncComponent(()=>import('../views/About'));
-// const List = asyncComponent(()=>import('../views/List'));
+const Hello = asyncComponent(()=>import('../views/Hello'));
+const Home = asyncComponent(()=>import('../views/Home'));
+const NotFound = asyncComponent(()=>import('../views/NotFound'));
+const About = asyncComponent(()=>import('../views/About'));
+const List = asyncComponent(()=>import('../views/List'));
 // function MyLoading(){
 //     return <div>Loading...</div>
 // }
