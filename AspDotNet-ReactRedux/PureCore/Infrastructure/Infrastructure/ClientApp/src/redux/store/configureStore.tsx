3945e4dd015b8fb393e0602da40cffd281b2ae1c
wuzinong <wuzinong@gmail.com>
2018-10-31 09:43:16 +0800

    update

--- AspDotNet-ReactRedux/PureCore/Infrastructure/Infrastructure/ClientApp/src/redux/store/configureStore.tsx
+++ AspDotNet-ReactRedux/PureCore/Infrastructure/Infrastructure/ClientApp/src/redux/store/configureStore.tsx
@@ -1,13 +1,18 @@
-import {createStore,applyMiddleware} from 'redux';
+import {compose, createStore,applyMiddleware} from 'redux';
 import reducers from '../reducers/index';
 // import {StoreState} from '../../types';
 import thunk from 'redux-thunk';
 import initState from './initState';
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export default function(){
     const store = createStore(
         reducers,
         initState,
-        applyMiddleware(thunk)
+        composeEnhancers(
+            applyMiddleware(thunk)
+        )
     );
     return store;
 }
