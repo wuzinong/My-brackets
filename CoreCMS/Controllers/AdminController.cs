fc78464c9f42156f321d50d86ea23de6f5c66f1b
wuzinong <wuzinong@gmail.com>
2019-08-28 17:31:33 +0800

    update

--- CoreCMS/Controllers/AdminController.cs
+++ CoreCMS/Controllers/AdminController.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCMS.Controllers
@@ -15,11 +20,54 @@ namespace CoreCMS.Controllers
             return View();
         }
 
+        [Authorize]
         [Route("secret/[controller]/list")]
         [HttpGet]
         public async Task<IActionResult> List()
         {
             return View();
+        }
+
+        [Route("secret/[controller]/login")]
+        [HttpPost]
+        public async Task<IActionResult> Login(string userName,string password,string returnUrl)
+        {
+            if (userName.Equals("admin") && password.Equals("12321"))
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, "MyUser"));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, "MyUserID"));
+                claims.Add(new Claim(ClaimTypes.Role, "MyRole"));
+
+
+                var identity = new ClaimsIdentity(claims, "CustomApiKeyAuth");
+
+                identity.AddClaim(new Claim(ClaimTypes.Name, "admin"));
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,new ClaimsPrincipal(identity));
+
+                //var principal = new ClaimsPrincipal(new[] { claimsIdentity });
+                //Thread.CurrentPrincipal = principal;
+                //HttpContext.Current.User = principal;
+
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    return Content("0");
+                }
+
+                return Content("1");
+            }
+
+           return Content("2");
+        }
+
+        [Route("secret/[controller]/logout")]
+        [HttpPost]
+        public async Task<IActionResult> Logout(string returnUrl)
+        {
+
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Content("2");
         }
     }
 }
--- CoreCMS/Startup.cs
+++ CoreCMS/Startup.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
@@ -20,6 +22,13 @@ namespace CoreCMS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, o =>
+                    {
+                        o.LoginPath = new PathString("/secret/admin/login");
+                        o.AccessDeniedPath = new PathString("/Error/Forbidden");
+                    });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the React files will be served from this directory
@@ -42,6 +51,8 @@ namespace CoreCMS
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
